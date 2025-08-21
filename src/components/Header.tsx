7cb562a6a6e3af6b0d1b868d619e5d111a9e18a1
Granit-700 <salymbaeverbol@gmail.com>
2025-08-21 15:51:30 +0600

    сделал Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,117 @@
+// Erbol
+import { AppBar, Box, Typography } from "@mui/material";
+import headerBg from "../assets/images/header_bg.png"; // твоя волна-фон
+import headerLogo from "../assets/icons/header_logo.svg";
+import headerBurger from "../assets/icons/header_burger.svg"; // иллюстрация бургера
+
+const Header = () => {
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        backgroundImage: `url(${headerBg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "100% 100%",     // полностью вмещает изображение
+        backgroundPosition: "center",  // по центру
+        backgroundColor: "transparent",
+        boxShadow: "none",
+        height: 466
+      }}
+    >
+      {/* Логотип */}
+      <Box
+        component="img"
+        src={headerLogo}
+        alt="YourMeal"
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          margin: "24px auto"
+        }}
+      />
+      {/* HeroBanner */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "21px"
+        }}
+      >
+        <Box
+          sx={{
+            width: 326,
+            height: 326,
+            padding: "26.74px 36.86px 9.31px 35.73px"
+
+          }}
+        >
+          <Box
+            component="img"
+            src={headerBurger}
+            alt="Burger"
+          />
+        </Box>
+        <Box sx={{ maxWidth: 500 }}>
+          <Typography
+            component="h1"
+            sx={{
+              fontFamily: "Nunito",
+              fontWeight: "800",
+              fontSize: "50px",
+              lineHeight: "120%",
+              letterSpacing: "0",
+              marginBottom: "54px",
+              color: "#fff"
+            }}
+          >
+            Только самые{" "}
+            <Typography
+              component="span"
+              sx={{
+                fontFamily: "Nunito",
+                fontWeight: 800,
+                fontSize: "50px",
+                lineHeight: "120%",
+                letterSpacing: 0,
+                color: "#FF5C00", // нужно выделить цветом
+              }}
+            >
+              сочные бургеры!
+            </Typography>
+          </Typography>
+
+          <Typography
+            component="p"
+            sx={{
+              fontFamily: "Nunito",
+              fontWeight: 400,
+              fontSize: "16px",
+              lineHeight: "100%",
+              letterSpacing: 0,
+              color: "#fff"
+            }}
+          >
+            Бесплатная доставка от{" "}
+            <Typography
+              component="span"
+              sx={{
+                fontFamily: "Nunito",
+                fontWeight: 600,
+                fontSize: "16px",
+                lineHeight: "100%",
+                letterSpacing: 0,
+                color: "#fff"
+              }}
+            >
+              599₽
+            </Typography>
+          </Typography>
+        </Box>
+      </Box>
+    </AppBar >
+  );
+};
+
+export default Header;
